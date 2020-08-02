f1fa62ad1651014f46ee8e0e9bf7cd93dd95de33
Aleck Tuluenga <aleckbryson@gmail.com>
2020-08-01 22:56:26 -0400

    Prompts added to JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,32 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Special characters for the function created
+const specialCharacters = "!@#$%^&*()";
+const generateButton = document.getElementById('generateBtn')
+generateButton.addEventListener('click', writePassword)
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  var alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  var alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  var numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-  var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?']
-  
-  
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Prompt user regarding password length and whether or not they would any Numbers, Lower/Uppercase letter, and any Special Characters
+function generatePassword() {
+  var passLength = prompt("Welcome to the Password Generator! Please enter the amount of characters you want for password. Please note that it must be at least 8 characters and less than 128 characters.");
+
+  var num = confirm("Would you like Numbers in your password?");
+
+  var lowerCase = confirm("Would you like Lowercase letters?");
+
+  var upperCase = confirm("Would you like UpperCase letters?");
+
+  var specChar = confirm("Lastly, would you like any Special Characters in your password?");
+
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+
+
+
+}
