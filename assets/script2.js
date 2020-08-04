0481c5113ad2d17160d7dc1654ce050b54aae907
Aleck Tuluenga <aleckbryson@gmail.com>
2020-08-03 20:24:11 -0400

    Typos deleted; unused variable removed; and comments adjusted to reflect code underneath

--- assets/script2.js
+++ assets/script2.js
@@ -1,29 +1,29 @@
-// Add event listener to generate button
-}
-// GIVEN I need a new, secure password
-// Assignment Code
-
-
+//Assignment Code
 var alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var speChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'];
 
-const generateButton = document.getElementById('generateBtn')
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generateBtn");
 
 generateBtn.addEventListener("click", writePassword);
 
-// WHEN I click the button to generate a password
+
+// WHEN prompted for password criteria
 function generatePassword() {
+    // THEN I select which criteria to include in the password
     var passLength = parseInt(prompt("Welcome to the Password Generator! Please enter the amount of characters you want for password. Please note that it must be at least 8 characters and less than 128 characters."));
-    // THEN I am presented with a series of prompts for password criteria
-    // check if they meet criteria 
+    
+    // WHEN prompted for the length of the password
+    // THEN I choose a length of at least 8 characters and no more than 128 characters
     if (confirm(passLength > 8 || passLength < 128)) {
         console.log(passLength);
         
         var passArr = []
+        var pass = ''
         
+        // WHEN prompted for character types to include in the password
+        // THEN I choose lowercase, uppercase, numeric, and/or special characters
         var num = confirm("Would you like Numbers in your password?");
         if (num === true) {
             passArr.push(numList);
@@ -43,8 +43,12 @@ function generatePassword() {
         if (specialChar === true) {
             passArr.push(speChar);
         }
+        // WHEN I answer each prompt
+        // THEN my input should be validated and at least one character type should be selected
         console.log(passArr, passLength)
         
+        // WHEN all prompts are answered
+        // THEN a password is generated that matches the selected criteria
         for (var i = 0; i < passLength; i++) {
             var randomChar = Math.floor(Math.random() * passArr.length);
             var randomPass = Math.floor(Math.random() * passArr[randomChar].length);
@@ -62,27 +66,13 @@ function generatePassword() {
     return pass
 }
 
-generatePassword (pass);
+// WHEN the password is generated
+generatePassword ();
 
-// Write password to the #password input
+// THEN the password is either displayed in an alert or written to the page
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     
     passwordText.value = password;
-
-
-
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+}
